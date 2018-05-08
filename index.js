b01ddbdc5a716d1aa607c1c283b36321e37deb42
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-08 16:01:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,6 @@ function showRepositories(event, data) {
   const repoList = template(repos)
   document.getElementById("repositories").innerHTML = repoList
 }
+document.addEventListener("DOMContentLoaded", function(event) {
+  Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
+});
